60393c07f5f6cd70516084e0534e5b68785cfe15
chiku binoy <chikubinoybinoy@gmail.com>
2024-07-02 21:27:33 +0530

    Your commit message

--- Frontend/app/src/AxiosCreate.js
+++ Frontend/app/src/AxiosCreate.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const SampleUrl = 'https://e-commercewebsite-uh5f.onrender.com';
+const SampleUrl = 'https://social-media-host-backends.onrender.com';
 const persistedLoginData = localStorage.getItem("persist:logindata");
 const loginData = persistedLoginData ? JSON.parse(persistedLoginData) : {};
 const loginInfo = loginData.login ? JSON.parse(loginData.login).LoginInfo[0] : null;
